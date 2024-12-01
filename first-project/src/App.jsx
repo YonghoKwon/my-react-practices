d548cc68ff16905679e382ebb76ee8a74823df04
Grandes <ho2708@naver.com>
2024-12-01 14:01:12 +0900

    [얄팍한 코딩사진 React강의 실습] :React 06강 - 컴포넌트 기초

--- first-project/src/App.jsx
+++ first-project/src/App.jsx
@@ -1,6 +1,19 @@
+import Child from "./Child.jsx";
+import {ArrowFunctionComp, ClassComp, FunctionComp} from "./Components.jsx";
+
+function Hello() {
+  return <h1>Hello, world!</h1>
+}
+
 function App() {
   return (
     <>
+      <Hello />
+      <h2>Hello, world!</h2>
+      <Child />
+      <ClassComp />
+      <FunctionComp />
+      <ArrowFunctionComp />
     </>
   )
 }
